f40beb444e00e93daacb18e21b9c0535a28d4851
unknown <daria_lash@naumenko-daria.pro>
2022-03-01 21:13:35 +0300

    first commit new branch

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    email = models.EmailField(primary_key=True, default='')
+    username = models.CharField(max_length=15)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
